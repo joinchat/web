40a1c8d8640be11ff0437eaad630f08de7b80df2
ermo4enkov <ermo4enkov@gmail.com>
2017-12-28 21:28:48 +0200

    Подключил MuiTheme Provider, разбираюсь как его нужно правильно имплементировать в новой версии

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+
+
 // import "./assets/scss/styles.scss";
 
 
 import { App } from "./views/App/app";
 
+
 ReactDOM.render(
     <App/>,
-    document.getElementById("example")
+    document.getElementById("root")
 );
--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -2,23 +2,15 @@ import * as React from "react";
 // import { Button } from "../../components/button/button";
 // import { ColorsPallet } from "../../blocks/colorspallet/colorspallete";
 // import { ButtonsCollection } from "../../blocks/buttonscollection/buttonscollection";
+import styled from "styled-components";
+import { MuiThemeProvider } from "material-ui/styles";
 
 
 export class App extends React.Component {
 
     render() {
-
         return(
-            <div className="storybook">
-                {/* <ColorsPallet/> */}
-                {/* <div className="storybook__element"> */}
-                    {/* <h1 className="header">Text</h1>
-                    <div>Font — Source Sans Pro</div>
-                    <h1>H1 - everyday</h1>
-                    <h2 className="subtitile">H2 - everyday</h2>
-                    <div>Text - everyday</div>
-                </div>
-                <ButtonsCollection/> */}
+            <div>
                 Hello!
             </div>
         );
